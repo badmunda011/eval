46dbac361f9e97bc739510ed04fd38437c3b2adc
coixo <vivansingh8372@outlook.com>
2025-01-03 18:25:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+import re
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+# Get this value from my.telegram.org/apps
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN")
