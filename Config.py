03e528a2d231e55f19f9760f92d141dabe6fcb96
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2025-04-21 19:16:46 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,3 +1,5 @@
 # Config.py
+API_ID = 25742938
+API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
 BOT_TOKEN = "8081277733:AAEmIxBGHhC96PfUcbrxvpNx4nfmXd-qc00"
 MONGO_URL = "mongodb+srv://BADMUNDA:BADMYDAD@badhacker.i5nw9na.mongodb.net/"  # ya Atlas URL if hosted
